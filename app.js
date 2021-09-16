8080caf9c7302c5191e2f3217e84de237e78b898
Alireza Soheili <71215827+alireza-constantin@users.noreply.github.com>
2021-09-16 15:19:54 +0430

    complete computer style

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const getdata = async () => {
 	try {
-		const res = await axios.get('https://api.github.com/users/defunkt');
+		const res = await axios.get('https://api.github.com/users/octocat');
 		console.log(res);
 	} catch (error) {
 		console.log(error);
